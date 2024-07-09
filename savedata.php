d4bd5b9c8c84fcea9b950ed49107df06511d3449
BIPIN SAHANI <bipinsahani786@gmail.com>
2024-07-09 14:12:19 +0530

    first commit

---
+++ savedata.php
@@ -0,0 +1,17 @@
+<?php
+echo $stu_name = $_POST['sname'];
+echo $stu_address = $_POST['saddress'];
+echo $stu_class = $_POST['class'];
+echo $stu_phone = $_POST['sphone'];
+
+include'config.php';
+
+$sql = "INSERT INTO student (sname,saddress,sclass,sphone) VALUES ('{$stu_name}','{$stu_address}','{$stu_class}','{$stu_phone}')";
+
+$result = mysqli_query($conn, $sql) or die("Query unsuccessful.");
+
+header("Location: http://localhost/crud_html/index.php");
+
+mysqli_close($conn);
+
+?>
